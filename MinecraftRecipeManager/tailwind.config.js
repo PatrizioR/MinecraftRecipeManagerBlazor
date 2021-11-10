6f916212bc98ebaa330f8332010bb0aa36643c7c
Patrizio Ricciardello <pricciardello@gmail.com>
2021-11-10 14:20:03 +0100

    Add project files.

---
+++ MinecraftRecipeManager/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    purge: {
+        enabled: false,
+        content: [
+            './**/*.html',
+            './**/*.razor'
+        ],
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
