41949837eaaba989c67b6871782f607f3349f0c2
Patrizio Ricciardello <pricciardello@gmail.com>
2021-11-14 15:52:02 +0100

    Add data project, rename folders

--- WasmClient/Program.cs
+++ WasmClient/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MinecraftRecipeManager;
+using MinecraftRecipeManager.WasmClient;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
